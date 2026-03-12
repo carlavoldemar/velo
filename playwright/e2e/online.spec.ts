9121144b3be00b95f08d905ca9a2eb2a4f722e5e
carlavoldemar <carlavoldemar@gmail.com>
2026-03-12 23:47:44 +0000

    Mudando o titulo do cenário

---
+++ playwright/e2e/online.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('a webapp deve estar online', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await expect(page).toHaveTitle(/Velo by Papito/)
+
+})
+
