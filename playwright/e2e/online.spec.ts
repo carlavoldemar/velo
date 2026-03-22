32223ff05f923f9c089645301154dbc40225374a
carlavoldemar <carlavoldemar@gmail.com>
2026-03-22 22:36:21 +0000

    iniciando a automação do teste de consulta de pedido aprovado

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,9 +1,11 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test('webapp deve estar online', async ({ page }) => {
-    await page.goto('http://localhost:5173')
+  await page.goto('http://localhost:5173/')
 
-    await expect(page).toHaveTitle(/Velo by Papito/)
-
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Velô by Papito/)
 })
+
+
 
