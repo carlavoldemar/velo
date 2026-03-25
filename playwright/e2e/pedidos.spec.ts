2d2555e101171ebb53de8f9188d18e66c64ae0aa
carlavoldemar <carlavoldemar@gmail.com>
2026-03-25 16:18:47 +0000

    Padrão AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,29 +1,26 @@
 import { test, expect } from '@playwright/test';
 
+///Padrão AAA - Arrange, Act, Assert = Preparar, Agir, Verificar
 test('deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
-
-  //Checkpoint - no selenium seria xpath="//h1[text]='Velô Sprint'"
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  //Checkpoint - no selenium seria xpath="//a[text()="Consultar Pedido" ou por cssSelector - a[href="/lookup"] ou por xpath -a[@href="/lookup"]
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
-
-  await page.getByTestId('search-order-id').fill('VLO-QY0HME');
-
   //Criado no codegen
-  await page.getByTestId('search-order-button').click();
 
+  //Arrange
+  await page.goto('http://localhost:5173/');
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint'); //Checkpoint - no selenium seria xpath="//h1[text]='Velô Sprint'"
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click(); 
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido'); //Checkpoint - no selenium seria xpath="//a[text()="Consultar Pedido" ou por cssSelector - a[href="/lookup"] ou por xpath -a[@href="/lookup"]
+
+  //Act
+  await page.getByTestId('search-order-id').fill('VLO-QY0HME');
+  await page.getByTestId('search-order-button').click(); 
+
+  //Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-QY0HME');
   await expect(page.getByTestId('order-result-status')).toBeVisible();
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
 
-  //Criado pela IA
   //await page.getByTestId('search-order-button').click();
- 
   //Checkpoint - no selenium seria xpath="//p[text()='Pedido Aprovado']"
   //await expect(page.getByRole('heading')).toContainText('Pedido Aprovado');
   //await expect(page.getByTestId('order-result-id')).toContainText('VLO-QY0HME');
